bde630057bc137db3a480bbd42f100c777778264
evymari <144184061+evymari@users.noreply.github.com>
2024-09-13 07:32:14 +0200

    [FP-2]-[FP-20]-feat create the backend logic with the front

--- api/urls.py
+++ api/urls.py
@@ -1,32 +1,9 @@
-"""
-URL configuration for api project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Ruta para el panel de administración de Django
-    path('api/auth/', obtain_auth_token, name='api_token_auth'),
-    # Ruta para obtener el token de autenticación
-    #path('api/users/', include('users.urls')),  # Incluye las rutas de la app 'users'
-    #path('api/photos/', include('photos.urls')),  # Incluye las rutas de la app 'photos'
-]
-
-
-
-"""from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView"""
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('users.urls')),  # Incluye las URLs de la app 'users'
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,33 +1,58 @@
-from rest_framework import status, permissions
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
-from .serializer import LoginSerializer, SignInSerializer  # Asegúrate de definir estos serializers
+from rest_framework.authtoken.models import Token
+from .models import CustomUser
+from .serializers import RegisterSerializer, LoginSerializer, UserDetailSerializer, UserUpdateSerializer
+from rest_framework import status
 
-class LoginView(APIView):
+class CustomLoginView(APIView):
     def post(self, request):
-        serializer = LoginSerializer(data=request.data)
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                'token': token.key,
+                'user': {
+                    'username': user.username,
+                    'email': user.email,
+                    'avatar': getattr(user, 'avatar', 'default_avatar.png'),
+                }
+            })
+        else:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
-            username = serializer.validated_data['username']
-            password = serializer.validated_data['password']
-            user = authenticate(username=username, password=password)
-            if user:
-                token, created = Token.objects.get_or_create(user=user)
-                return Response({'token': token.key}, status=status.HTTP_200_OK)
-            return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class SignInView(APIView):
-    def post(self, request):
-        serializer = SignInSerializer(data=request.data)
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserDetailSerializer(request.user)
+        return Response(serializer.data)
+
+    def put(self, request):
+        serializer = UserUpdateSerializer(request.user, data=request.data, partial=True)
         if serializer.is_valid():
-            username = serializer.validated_data['username']
-            password = serializer.validated_data['password']
-            user = authenticate(username=username, password=password)
-            if user:
-                token, created = Token.objects.get_or_create(user=user)
-                return Response({'token': token.key}, status=status.HTTP_200_OK)
-            return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class DeleteUserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request):
+        user = request.user
+        user.delete()
+        return Response({"message": "User deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
